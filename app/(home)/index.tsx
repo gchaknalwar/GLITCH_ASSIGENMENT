f6de3b3af1738f83cd2ac15693880f7ac19bf642
gchaknalwar <114746290+gchaknalwar@users.noreply.github.com>
2025-04-06 00:37:22 +0530

    Add files via upload

---
+++ app/(home)/index.tsx
@@ -0,0 +1,23 @@
+import { Link } from 'expo-router';
+import { View, Text, StyleSheet } from 'react-native';
+
+export default function HomeScreen() {
+  return (
+    <View style={styles.container}>
+      <Link
+        href={{
+          pathname: "/details",
+          params: { id: 'bacon' },
+        }}>
+      </Link>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
---
+++ app/_layout.tsx
@@ -0,0 +1,10 @@
+// import { Stack } from 'expo-router';
+
+// export default function RootLayout(){
+//   return (
+//     <Stack>
+//       <Stack.Screen name="Home" />
+//       <Stack.Screen name="details" />
+//     </Stack>
+//   );
+// }
